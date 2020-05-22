aa05899230b3b18802f9f7aba05821a42b844ff8
René H <hasertrene@gmail.com>
2020-05-22 15:48:21 +0200

    feat: No more overlap in EVENTS, for validation for the date and time

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Event } from '../models/Event';
+import { format, isWithinInterval } from 'date-fns';
 
 @Injectable({
   providedIn: 'root',
@@ -47,6 +48,27 @@ export class EventService {
   }
 
   addEvent(event) {
-    this.events.push(event);
+    const sameHour: any = this.events.filter((e) => {
+      return (
+        format(new Date(e.start), 'yyyy-MM-dd  HH') ===
+          format(new Date(event.start), 'yyyy-MM-dd  HH') ||
+        format(new Date(event.end), 'yyyy-MM-dd  HH') ===
+          format(new Date(e.end), 'yyyy-MM-dd  HH')
+      );
+    });
+    console.log(sameHour);
+    const conflict: any = sameHour.filter((e) => {
+      isWithinInterval(new Date(e.start), {
+        start: new Date(event.start),
+        end: new Date(event.end),
+      }) ||
+        isWithinInterval(new Date(e.end), {
+          start: new Date(event.start),
+          end: new Date(event.end),
+        });
+    });
+    conflict
+      ? alert('Er staat al een afspraak op dit tijdstip.')
+      : this.events.push(event);
   }
 }
