0a85923ec5b9ae28455206033fcf8fd540ffec76
René H <hasertrene@gmail.com>
2020-05-24 22:32:11 +0200

    bug: week starts on 1

--- src/app/views/calendar/calendar.component.ts
+++ src/app/views/calendar/calendar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../../models/Event';
 import { EventService } from '../../services/event.service';
-import { getWeek, addWeeks, subWeeks } from 'date-fns';
+import { getWeek, addWeeks, subWeeks, startOfWeek } from 'date-fns';
 import { DAYS_OF_WEEK } from 'angular-calendar';
 
 @Component({
@@ -13,9 +13,7 @@ export class CalendarComponent implements OnInit {
   events: Event[];
   viewDate: Date = new Date();
   locale: string = 'nl';
-  title: string = 'Dr. Dokter';
-  weekNumber = getWeek(this.viewDate);
-  view: string = 'month';
+  weekNumber = getWeek(this.viewDate, { weekStartsOn: 1 });
   weekStartsOn: number = 1;
   weekendDays: number[] = [DAYS_OF_WEEK.SATURDAY, DAYS_OF_WEEK.SUNDAY];
 
@@ -30,10 +28,10 @@ export class CalendarComponent implements OnInit {
   }
   addWeek() {
     this.viewDate = addWeeks(this.viewDate, 1);
-    this.weekNumber = getWeek(this.viewDate);
+    this.weekNumber = getWeek(this.viewDate, { weekStartsOn: 1 });
   }
   subWeek() {
     this.viewDate = subWeeks(this.viewDate, 1);
-    this.weekNumber = getWeek(this.viewDate);
+    this.weekNumber = getWeek(this.viewDate, { weekStartsOn: 1 });
   }
 }
