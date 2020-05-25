dec70e7a07327b68e7365ea0c2f7e1fbaeb8a080
René H <hasertrene@gmail.com>
2020-05-25 14:24:28 +0200

    chore: day starts and ends at 7

--- src/app/components/event-item/event-item.component.ts
+++ src/app/components/event-item/event-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Event } from '../../models/Event';
-import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-event-item',
@@ -10,8 +9,6 @@ import { EventService } from '../../services/event.service';
 export class EventItemComponent implements OnInit {
   @Input() event: Event;
   @Output() deleteEvent: EventEmitter<Event> = new EventEmitter();
-
-  constructor(private eventService: EventService) {}
 
   ngOnInit(): void {}
 
